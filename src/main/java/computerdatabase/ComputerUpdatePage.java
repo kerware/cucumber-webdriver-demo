ea13c1235decdde8392773031ae8754a4db06942
kerware <olivier.charles@kerware.com>
2023-11-30 12:22:29 +0100

    Adding methods to ComputerUpdatePage.java

--- src/main/java/computerdatabase/ComputerUpdatePage.java
+++ src/main/java/computerdatabase/ComputerUpdatePage.java
@@ -4,31 +4,46 @@ import testframework.WebOperations;
 
 public class ComputerUpdatePage {
 	
-	private WebOperations _webOp = null;
+	private WebOperations _webOps = null;
 	
 	public ComputerUpdatePage( WebOperations webOp ) {
-		_webOp = webOp;
+		_webOps = webOp;
 	}
 	
 	public void updateComputerForm( String name, String introDate, String disDate, String company ) {
+		if ( name != null ) {
+			_webOps.type("txtName", name , false);
+		}
+		if ( introDate != null ) {
+			_webOps.type("txtDateIntro", introDate , false);
+		}
+		if ( disDate != null ) {
+			_webOps.type("txtDateDis", disDate , false);
+		}
+		if ( company != null ) {
+			_webOps.selectOptionByText("selectCompany", company);
+		}
 		
 	}
 	
 	public void submitComputerForm() {
-		
+		_webOps.click("btnSaveComputer");
 	}
 	
 	public void updateComputer(  String name, String introDate, String disDate, String company ) {
+		updateComputerForm( name, introDate, disDate, company );
+		submitComputerForm();
 		
 	}
 	
 	public void cancelUpdate( ) {
-		
+		_webOps.click("btnCancel");
 	}
 
-	public boolean verifyHelpMessage( String helpMessage ) {
-		boolean result = false;
-		return result;
+	public void deleteComputer() {
+		_webOps.click("btnDeleteComputer");
 	}
+
+
 
 }
